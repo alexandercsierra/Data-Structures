fa78db306537b6c7bac0cffdd7ebea1a25b974af
Alexander Sierra <AlexanderCSierra@alexanderrasmbp.lan>
2020-05-11 15:36:06 -0400

    stack tests passing with linked list

--- stack/stack.py
+++ stack/stack.py
@@ -10,17 +10,89 @@ return elements in Last In First Out order.
 3. What is the difference between using an array vs. a linked list when 
    implementing a Stack?
 """
+# class Stack:
+#     def __init__(self):
+#         self.size = 0
+#         self.storage = []
+
+#     def __len__(self):
+#         return len(self.storage)
+
+#     def push(self, value):
+#         self.storage.append(value)
+
+#     def pop(self):
+#         if len(self.storage) > 0:
+#             return self.storage.pop()
+
 class Stack:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        self.storage = LinkedList()
 
     def __len__(self):
-        return len(self.storage)
+        return self.storage.length
 
     def push(self, value):
-        self.storage.append(value)
+        self.storage.add_to_end(value)
 
     def pop(self):
         if len(self.storage) > 0:
-            return self.storage.pop()
+            return self.storage.remove_from_end()
+
+
+class Node:
+  def __init__(self, value=None, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next(self):
+    return self.next_node
+
+  def set_next(self, new_next):
+    self.next_node = new_next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def __len__(self):
+        return self.length
+
+    def add_to_end(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+            self.tail = new_node
+            self.length += 1
+        else:
+            self.tail.next_node=new_node
+            self.tail = new_node
+            self.length += 1
+
+    def remove_from_end(self):
+        if self.length > 0:
+            current = self.head
+            previous = None
+            while current.next_node != None:
+                previous = current
+                current = current.get_next()
+            if previous != None:
+                previous.set_next(None)
+                self.tail = previous
+                self.length -= 1
+                return current.value
+            else:
+                self.head = None
+                self.tail = None
+                self.length = 0
+                return current.value
+        else:
+            return None
+
+        
